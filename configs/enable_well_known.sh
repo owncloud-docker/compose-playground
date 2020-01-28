412c04e6a1ea28e4000569b4df17d85813d48216
Robert Kaussow <mail@geeklabor.de>
2020-01-28 14:39:38 +0100

    fix content type

--- configs/enable_well_known.sh
+++ configs/enable_well_known.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -xe
 
+sed -i 's/# Let browsers cache CSS, JS files for half a year/<Files apple-app-site-association>\n    Header set Content-type application\/json\n  <\/Files>/g' /var/www/owncloud/.htaccess
 sed -i 's/\/(acme-challenge|pki-validation)//g' /var/www/owncloud/.htaccess
 
 true
