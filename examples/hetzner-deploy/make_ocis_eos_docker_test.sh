a710031cdaf0f72a9d9e61567d8f322dd4df20e8
Juergen Weigert <jnweiger@gmail.com>
2020-05-27 15:56:19 +0200

    pre-dploy some files to user einstein folder 'init'

--- examples/hetzner-deploy/make_ocis_eos_docker_test.sh
+++ examples/hetzner-deploy/make_ocis_eos_docker_test.sh
@@ -18,6 +18,11 @@ if [ -z "$IPADDR" ]; then
   exit 1;
 fi
 
+ocis_version=v1.0.0-beta4
+eos_home=/eos/dockertest/reva/users/e/einstein
+eos_uid=20000
+eos_gid=30000
+
 LOAD_SCRIPT <<EOF
   svcfile=/usr/lib/systemd/system/docker.service			# ubuntu-20.04
   test -e \$svcfile || svcfile=/lib/systemd/system/docker.service	# ubuntu-18.04
@@ -32,11 +37,19 @@ LOAD_SCRIPT <<EOF
   ## make sure we build beta4. CAUTION: if the selected tag or branch does not exist, we get this error:
   ##  Pulling ocis (cloudservices/eos/eos-citrine-ocis:test)...
   ##  ERROR: The image for the service you're trying to recreate has been removed
-  sed -i -e "s/ocis.git/ocis.git -b v1.0.0-beta4/g" containers/Dockertmp.ocis
+  sed -i -e "s/ocis\.git.*$/ocis.git -b $ocis_version/g" containers/Dockertmp.ocis
 
   ./build -a $IPADDR -t test
   ./setup -a
-  # docker-compose up ocis &	# stop and restart, this time with log output visible.
+
+  if [ -f ~/make_machine.bashrc ]; then
+    # make that file appear within the owncloud
+    docker cp ~/make_machine.bashrc ocis:/
+    docker exec ocis eos -r 0 0 mkdir -p $eos_home/init
+    docker exec ocis eos -r 0 0 chown -r $eos_uid:$eos_gid $eos_home
+    docker exec ocis eos -r $eos_uid $eos_gid cp make_machine.bashrc $eos_home/init
+    docker exec ocis eos -r $eos_uid $eos_gid touch $eos_home/init/this-is-ocis-$ocis_version
+  fi
 
   sleep 5
   cat <<EOM
@@ -47,6 +60,10 @@ LOAD_SCRIPT <<EOF
 # Connect your browser or client to
 
    https://$IPADDR:9200
+
+# View the logs with
+
+   ssh root@$IPADDR sh -c "echo; cd compose-playground/examples/eos-docker; docker-compose logs -f"
 
 # Follow the instructions at
 
