f95d60c23c482ea5d5273dad9ad46f00db780dcc
Jan Müller <jan.mueller@catdev.io>
2020-10-05 10:36:46 +0200

    Set valid url for openid config oc10

--- examples/oc10-with-ocis-infra/owncloud/overlay/etc/templates/config.php
+++ examples/oc10-with-ocis-infra/owncloud/overlay/etc/templates/config.php
@@ -38,18 +38,14 @@ function getConfigFromEnv() {
         'loginButtonName' => 'OpenId Connect',
         'search-attribute' => 'preferred_username',
         'mode' => 'userid',
-
-        // Manual config if no auto-discovery is available
-        /*
         'provider-params' => [
-          "authorization_endpoint" => "http://ocis:9130/signin/v1/identifier/_/authorize",
-          "token_endpoint" => "http://ocis:9130/konnect/v1/token",
-          "userinfo_endpoint"=> "http://ocis:9130/konnect/v1/userinfo",
-          "end_session_endpoint" => "http://ocis:9130/signin/v1/identifier/_/endsession",
-          "check_session_iframe" => "http://ocis:9130/konnect/v1/session/check-session.html",
-          "jwks_uri" => "http://ocis:9130/konnect/v1/jwks.json"
+          "authorization_endpoint" => "https://ocis-bridge.owncloud.works//signin/v1/identifier/_/authorize",
+          "token_endpoint" => "https://ocis-bridge.owncloud.works//konnect/v1/token",
+          "userinfo_endpoint"=> "https://ocis-bridge.owncloud.works//konnect/v1/userinfo",
+          "end_session_endpoint" => "https://ocis-bridge.owncloud.works//signin/v1/identifier/_/endsession",
+          "check_session_iframe" => "https://ocis-bridge.owncloud.works//konnect/v1/session/check-session.html",
+          "jwks_uri" => "https://ocis-bridge.owncloud.works//konnect/v1/jwks.json"
         ]
-        */
       ],
     'datadirectory' => getenv('OWNCLOUD_VOLUME_FILES'),
     'dbtype' => getenv('OWNCLOUD_DB_TYPE'),
