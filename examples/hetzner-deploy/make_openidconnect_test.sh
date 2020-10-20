c3e79d38507fe2bf660034221aba174a33ecf29d
Juergen Weigert <jnweiger@gmail.com>
2020-10-20 15:24:08 +0200

    generate openid machines with timestamp in dnsname

--- examples/hetzner-deploy/make_openidconnect_test.sh
+++ examples/hetzner-deploy/make_openidconnect_test.sh
@@ -6,16 +6,19 @@
 #  https://github.com/owncloud-docker/compose-playground/blob/master/compose/kopano/konnect/README.md
 #  https://doc.owncloud.com/server/10.5/admin_manual/configuration/user/oidc/
 #
-# CAUTION: Do not use the obvious README in https://github.com/owncloud/openidconnect/ - it is wrong.
+# CAUTION: https://github.com/owncloud/openidconnect/ has a README.md
+#		- some concepts there are correct, e.g. index.php/apps/openidconnect/config rewrite
+#		- some other parts may need verification.
 #
 # 2020-10-16, jw@owncloud.com
+# 2020-11-20, jw@owncloud.com
 #
 
 echo "Estimated setup time: 8 minutes ..."
 
 vers=1.0.0RC5
 oauth2_vers=0.4.4RC1
-d_vers=$(echo $vers  | tr '[A-Z]' '[a-z]' | tr . -)
+d_vers=$(echo $vers  | tr '[A-Z]' '[a-z]' | tr . -)-$(date +%Y%m%d)
 source ./make_machine.sh -u openidconnect-$d_vers-test -p git,screen,docker.io,docker-compose
 
 comp_yml=kopano/konnect/docker-compose.yml
