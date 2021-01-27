643ed3372b3505639de7d34be517b9f9a1ff4b79
Juergen Weigert <jnweiger@gmail.com>
2021-01-27 13:02:27 +0100

    give docker container a name

--- examples/hetzner-deploy/make_oc10_server.sh
+++ examples/hetzner-deploy/make_oc10_server.sh
@@ -12,10 +12,12 @@ source lib/make_machine.sh -u oc-$d_vers -p git,screen,docker.io,wget
 HTTP_PORT=80
 
 INIT_SCRIPT << EOF
-  mkdir -p /tmp/ocmount
-  docker run -d --rm -v /tmp/ocmount:/mnt/data -p $HTTP_PORT:8080 owncloud/server:$vers &
+  mkdir -p /root/ocmount
+  docker run --name oc-$d_vers -d --rm -v /root/ocmount:/mnt/data -p $HTTP_PORT:8080 owncloud/server:$vers
   
-  echo "Try: docker logs -f \$(docker ps -q)"
-  echo "Try: docker exec \$(docker ps -q) bash"
+  echo "Try: docker logs -f oc-$d_vers"
+  echo "Try: docker exec -ti oc-$d_vers bash"
   echo "Try: firefox http://$IPADDR:80"
+  echo ""
+  echo "You can find config, apps, and data folders under /root/ocmount"
 EOF
