344e2301215776820556286966e9a9204240fb62
Juergen Weigert <jnweiger@gmail.com>
2021-07-13 23:12:14 +0200

    make_redis_cluster.sh can produce a remote sharded redis cluster and proposes an owncloud config snippet for its use.

--- examples/hetzner-deploy/make_oc10_apps.sh
+++ examples/hetzner-deploy/make_oc10_apps.sh
@@ -19,8 +19,8 @@ vers=10.8.0beta2
 tar=https://download.owncloud.org/community/owncloud-complete-20210326.tar.bz2
 test -n "$OC_VERSION" && vers="$OC_VERSION"
 test -n "$OC10_VERSION" && vers="$OC10_VERSION"
-test "$vers" = "10.8.0-beta2" -o "$vers" = "10.8.0beta1" && tar=https://download.owncloud.org/community/testing/owncloud-complete-20210705.tar.bz2
-test "$vers" = "10.8.0-beta1" -o "$vers" = "10.8.0beta2" && tar=https://download.owncloud.org/community/testing/owncloud-complete-20210621.tar.bz2
+test "$vers" = "10.8.0-beta2" -o "$vers" = "10.8.0beta2" && tar=https://download.owncloud.org/community/testing/owncloud-complete-20210705.tar.bz2
+test "$vers" = "10.8.0-beta1" -o "$vers" = "10.8.0beta1" && tar=https://download.owncloud.org/community/testing/owncloud-complete-20210621.tar.bz2
 test "$vers" = "10.7.0"       -o "$vers" = "10.7"        && tar=https://download.owncloud.org/community/owncloud-complete-20210326.tar.bz2
 test "$vers" = "10.6.0"       -o "$vers" = "10.6"        && tar=https://download.owncloud.org/community/owncloud-complete-20201216.tar.bz2
 test "$vers" = "10.5.0"       -o "$vers" = "10.5"        && tar=https://download.owncloud.org/community/owncloud-complete-20200731.tar.bz2
--- examples/hetzner-deploy/make_redis_cluster.sh
+++ examples/hetzner-deploy/make_redis_cluster.sh
@@ -18,13 +18,14 @@
 
 test -z "$REDIS_CLUSTER_PASS"        && REDIS_CLUSTER_PASS="$(tr -dc 'a-z0-9' < /dev/urandom | head -c 32)"
 test -z "$REDIS_CLUSTER_MASTER_PASS" && REDIS_CLUSTER_MASTER_PASS="$REDIS_CLUSTER_PASS"
-test -z "$REDIS_CLUSTER_PORT_BASE"   && REDIS_CLUSTER_PORT_BASE=7000
+test -z "$REDIS_CLUSTER_PORT_BASE"   && REDIS_CLUSTER_PORT_BASE=7650
 
 
 source lib/make_machine.sh -u redis-cl-$(date +%Y%m%d) -p screen,redis-server "$@"
 
 INIT_SCRIPT << EOF
 set -x
+service redis stop # shut down the default redis-server at port 6379, it is not part of our cluster.
 echo never > /sys/kernel/mm/transparent_hugepage/enabled
 sysctl vm.overcommit_memory=1
 for i in 0 1 2; do
@@ -46,21 +47,30 @@ EOCONF
 done
  sleep 3
  export REDISCLI_AUTH=$REDIS_CLUSTER_PASS
- echo yes | redis-cli --cluster create "127.0.0.1:$REDIS_CLUSTER_PORT_BASE" "127.0.0.1:$((REDIS_CLUSTER_PORT_BASE+1))" "127.0.0.1:$((REDIS_CLUSTER_PORT_BASE+2))"
+ echo yes | redis-cli --cluster create "$IPADDR:$REDIS_CLUSTER_PORT_BASE" "$IPADDR:$((REDIS_CLUSTER_PORT_BASE+1))" "$IPADDR:$((REDIS_CLUSTER_PORT_BASE+2))"
  sleep 5
+ redis-cli -c -p $REDIS_CLUSTER_PORT_BASE cluster info
+ redis-cli -c -p $REDIS_CLUSTER_PORT_BASE cluster nodes
  redis-cli -c -p $REDIS_CLUSTER_PORT_BASE set y 100
  redis-cli -c -p $REDIS_CLUSTER_PORT_BASE set z 200
- redis-cli -c -p $REDIS_CLUSTER_PORT_BASE get y
- redis-cli -c -p $REDIS_CLUSTER_PORT_BASE get z
+sleep 3
+ for p in $REDIS_CLUSTER_PORT_BASE $((REDIS_CLUSTER_PORT_BASE+1)) $((REDIS_CLUSTER_PORT_BASE+2)); do redis-cli -c -p \$p keys '*'; done
+sleep 1
  cat <<EOM
  # The 3 nodes redis cluster is ready at $IPADDR:$REDIS_CLUSTER_PORT_BASE
- # Add this to your owncloud/config/redis_cl.php to use the cluster:
+ # Add this to your owncloud/config/redis_cl.config.php to use the cluster:
+<?php
+// APCu is faster at local caching than Redis. If you have enough memory, use APCu for memory caching and Redis for file locking.
+// If you are low on memory, use Redis for both
 \\\$CONFIG = array (
+ 'memcache.distributed' => '\OC\Memcache\Redis',
+ 'memcache.locking' => '\OC\Memcache\Redis',
+ 'memcache.local' => '\OC\Memcache\Redis',
  'redis.cluster' => [
 	'seeds' => [ // provide some/all of the cluster servers to bootstrap discovery, port required
 	  '$IPADDR:$REDIS_CLUSTER_PORT_BASE',
-	  '$IPADDR:$((REDIS_CLUSTER_PORT_BASE+1))'
-	  '$IPADDR:$((REDIS_CLUSTER_PORT_BASE+2))'
+	  '$IPADDR:$((REDIS_CLUSTER_PORT_BASE+1))',
+	  '$IPADDR:$((REDIS_CLUSTER_PORT_BASE+2))',
 	],
 	'timeout' => 0.0,
 	'read_timeout' => 0.0,
